4285dd3d946bbd635d6d758a03e7cd22afa02e2f
Bota <bota87@users.noreply.github.com>
2025-04-25 15:45:58 +0200

    gestito switch per pandorone

--- bitcoinSwitch/config-example.h
+++ bitcoinSwitch/config-example.h
@@ -5,4 +5,5 @@ String wifiPassword = "password";
 String lnbitsServer = "lnbits.com";
 String WalletReadKey = "xxxxxxxx"; // Invoice/read key for the LNbits wallet you want to watch
 int switchPIN = 13;
+int tvPIN = 12; // PIN Accensione TV
 int durationms = 300;
