b6aa2f6763e16d72c2e4faf387be74199a064217
Bota <bota87@users.noreply.github.com>
2025-04-27 22:52:27 +0200

    modifiche per biliardino

--- bitcoinSwitch/bitcoinSwitch.cpp
+++ bitcoinSwitch/bitcoinSwitch.cpp
@@ -1,7 +1,8 @@
-#include "config-pandorino.h"
+#include "config-biliardino.h"
 #include <WiFi.h>
 #include <ArduinoJson.h>
 #include <WebSocketsClient.h>
+#include <Servo.h>
 
 String payloadStr = "";
 bool paid = false;
@@ -13,10 +14,13 @@ unsigned long lastMessageTime = 0;
 const unsigned long pingInterval = 10000;       // ogni 10s invia ping
 const unsigned long maxSilentTime = 30000;      // se non riceve nulla per 30s, considera la connessione persa
 
-
 WebSocketsClient webSocket;
+Servo servo1;
 
-void ElaboraPagamento();
+void ElaboraPagamento(bool &openDoor, bool &turnLightOn, int &durationms);
+void ControllaSpengimentoLuci();
+void OpenDoor();
+void TurnLightOn(int durationms);
 void webSocketEvent(WStype_t type, uint8_t *payload, size_t length);
 
 void setup()
@@ -24,18 +28,34 @@ void setup()
     Serial.begin(115200);
     Serial.println("Welcome to BitcoinSwitch");
 
-    pinMode(LED_BUILTIN, OUTPUT); // To blink on board LED
+    pinMode(LED_BUILTIN, OUTPUT);   // To blink on board LED
+    pinMode(openPin, INPUT_PULLUP); // bottone per simulare il pagamento del gettone e dell'illuminazione
+    pinMode(lightPin, OUTPUT);      // per accendere le luci
+
+    if (servo1.attach(servoPin))
+        Serial.println("Servo attached on pin: " + String(servoPin));
+    else
+    {
+        Serial.println("ERROR: failed to attach servo on pin " + String(servoPin));
+        while (true) // se non ho trovato il servo blocco l'esecuzione del programma e faccio lampeggiare velocemente il led
+        {
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(100);
+            digitalWrite(LED_BUILTIN, LOW);
+            delay(100);
+        }
+    }
 
     WiFi.begin(ssid.c_str(), wifiPassword.c_str());
     Serial.print("Connecting to WiFi");
     while (WiFi.status() != WL_CONNECTED)
     {
         Serial.print(".");
-        delay(500);
         digitalWrite(LED_BUILTIN, HIGH);
-        Serial.print(".");
         delay(500);
+        Serial.print(".");
         digitalWrite(LED_BUILTIN, LOW);
+        delay(500);
     }
 
     // Use in threshold mode
@@ -73,18 +93,56 @@ void loop()
         lastMessageTime = now; // resetto altrimenti disconnette in continuazione
     }
 
-    if (!paid)
-        return;
+    bool openDoor = false, turnLightOn = false;
+    int durationms = 0;
+    
+    if (paid)
+    {
+        paid = false;
+        Serial.println("Paid");
 
-    paid = false;
-    Serial.println("Paid");
+        digitalWrite(LED_BUILTIN, LOW); // durante l'elaborazione del pagamento spengo il led (si riaccende all'inizio del loop con lo stato del websocket)
+        ElaboraPagamento(/*byref*/ openDoor, /*byref*/ turnLightOn, /*byref*/ durationms);
+    }
+    else if (digitalRead(openPin) == LOW)
+    {
+        Serial.println("Forzata apertura e illuminazione");
+        openDoor = true;
+        turnLightOn = true;
+        durationms = (10 + alertSecondsBeforeOff) * 1000;
+    }
 
-    digitalWrite(LED_BUILTIN, LOW); // durante l'elaborazione del pagamento spengo il led
-    ElaboraPagamento();
-    digitalWrite(LED_BUILTIN, HIGH);
+    ControllaSpengimentoLuci();
+
+    if (turnLightOn)
+        TurnLightOn(durationms);
+
+    if (openDoor)
+        OpenDoor();
 }
 
-void ElaboraPagamento()
+void ControllaSpengimentoLuci()
+{
+    if (lightOffTime == 0)
+        return;
+    unsigned int time = millis();
+    if (lightAlertTime > 0 && time > lightAlertTime)
+    {
+        Serial.println("Tra " + String(alertSecondsBeforeOff) + " secondi spengo le luci");
+        digitalWrite(lightPin, LOW);
+        delay(500);
+        digitalWrite(lightPin, HIGH);
+        lightAlertTime = 0;
+    }
+    else if (time > lightOffTime)
+    {
+        Serial.println("Spengo le luci");
+        digitalWrite(lightPin, LOW);
+        lightOffTime = 0;
+    }
+}
+
+void ElaboraPagamento(bool &openDoor, bool &turnLightOn, int &durationms)
 {
     JsonDocument doc;
     DeserializationError error = deserializeJson(doc, payloadStr);
@@ -99,11 +157,39 @@ void ElaboraPagamento()
     JsonObject extra = payment["extra"];
     double paidAmount = extra["wallet_fiat_amount"];
     Serial.println("paidAmount: " + String(paidAmount));
+    String lnaddress = extra["lnaddress"];
+    Serial.println("lnaddress: " + lnaddress);
 
-    pinMode(switchPIN, OUTPUT);
-    digitalWrite(switchPIN, HIGH);
-    delay(durationms);
-    digitalWrite(switchPIN, LOW);
+    if (lnaddress.startsWith(lnAddressGettone + "@"))
+        openDoor = true;
+    else
+    {
+        turnLightOn = true;
+        durationms = (paidAmount / lightPricePerMinute) * 60 * 1000;
+        Serial.println("durationms: " + String(durationms));
+    }
+}
+
+void TurnLightOn(int durationms)
+{
+    if (lightOffTime == 0)
+        lightOffTime = millis();
+    lightOffTime += durationms;
+    lightAlertTime = lightOffTime - alertSecondsBeforeOff * 1000;
+    Serial.println("Accendo le luci per " + String(durationms) + " ms");
+    digitalWrite(lightPin, HIGH);
+    delay(500);
+}
+
+void OpenDoor()
+{
+    servo1.write(openDegree);
+    Serial.println("Servo aperto: " + String(openDegree));
+
+    delay(2000);
+
+    servo1.write(closeDegree);
+    Serial.println("Servo chiuso: " + String(closeDegree));
 }
 
 //////////////////WEBSOCKET///////////////////
